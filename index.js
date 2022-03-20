d3b8aba9f027d98ab72524f7e368efe8bccafc9e
codrinaignat <94629883+codrinaignat@users.noreply.github.com>
2022-03-20 11:10:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('javascript is cool');
+var number = 10;
+var string = 'hello box';
+var isCool = true; 
+
+document.getElementById('box').innerHTML = string;
+document.getElementById('box').outerHTML = number; 
